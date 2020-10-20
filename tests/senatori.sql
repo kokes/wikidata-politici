bed85c4f09618f55aa5ff9eeaecc88cd5dd8a6a2
Ondrej Kokes <ondrej.kokes@gmail.com>
2020-10-20 17:21:36 +0200

    lepsi parovani senatoru v testech

--- tests/senatori.sql
+++ tests/senatori.sql
@@ -6,9 +6,9 @@ select
    lag(wikidata_id) over(partition by obvod order by od asc) preceded_by,
    lead(wikidata_id) over(partition by obvod order by od asc) succeeded_by
 from v_poslanci_organy org
-left join poslanci_osoba_extra ex on ex.id_osoba = org.id_osoba
+inner join poslanci_osoba_extra ex on ex.id_osoba = org.id_osoba
 inner join v_ids on v_ids.psp_id = org.id_osoba
 where nazev_organu_cz = 'Senát' -- and obvod = 5
 group by org.id_osoba, od
-order by prijmeni, jmeno, od
+order by od desc
 limit 1000;
